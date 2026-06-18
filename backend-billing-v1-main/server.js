0ad52aff9e8944c3301a4056b0818b23843a0d94
ishaq <isaqammar2004@gmail.com>
2026-06-18 17:14:38 +0530

    fixed cors

--- backend-billing-v1-main/server.js
+++ backend-billing-v1-main/server.js
@@ -135,4 +135,4 @@ app.get('/swagger.json', (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}` ))
